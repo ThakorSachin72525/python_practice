bddf23194da81b65efbbe43897faa5bc6da4ad31
Sachin Thakor <sachinthakor722446@gmail.com>
2025-07-28 01:11:36 +0530

    os.path time size format

---
+++ path_interaction.py
@@ -0,0 +1,17 @@
+import os
+from pathlib import Path
+from datetime import datetime
+
+p = Path.cwd()
+
+# print(p)
+
+for root, dirs, files in os.walk(p):
+    for file in files:
+        if file.endswith("ipynb"):
+            full_path = os.path.join(root, file)
+            size = os.path.getsize(full_path) / 1024
+            t = os.path.getmtime(full_path)
+            dt = datetime.fromtimestamp(t)
+            formatted = dt.strftime("%d/%m/%Y %I:%M:%S %p")
+            print(f"Path{full_path} || Size:{size: .2f} kb || Time: {formatted}")
