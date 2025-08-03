f27076b8c66c5980f35fe4fbf7e040bf62b31b82
Sachin Thakor <sachinthakor722446@gmail.com>
2025-08-03 13:08:23 +0530

    SQL Query updated

--- SQL_Alchemy/1.sql
+++ SQL_Alchemy/1.sql
@@ -1,1 +1,16 @@
-SELECT TOP 10 * FROM Person.PersonPhone
+SELECT
+    p.FirstName,
+    p.MiddleName,
+    p.LastName,
+    e.JobTitle,
+    e.HireDate,
+    ea.EmailAddress,
+    pp.PhoneNumber
+FROM
+    Person.Person AS p
+JOIN
+    HumanResources.Employee AS e ON p.BusinessEntityID = e.BusinessEntityID
+LEFT JOIN
+    Person.EmailAddress AS ea ON p.BusinessEntityID = ea.BusinessEntityID
+LEFT JOIN
+    Person.PersonPhone AS pp ON p.BusinessEntityID = pp.BusinessEntityID;
